f7f73e6dc65835ce320372e1f8d2abd1264c7ca7
q67991548 <levonyuk.888@gmail.com>
2026-03-07 17:40:59 +0300

    07.03

--- src/app/pages/configurator/step-4/step-4.component.ts
+++ src/app/pages/configurator/step-4/step-4.component.ts
@@ -4,6 +4,7 @@ import {Select} from 'primeng/select';
 import {Material} from '../../../_services/wardrobe-params.service';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {Popover} from 'primeng/popover';
+import {CabinetConfiguratorService} from '../../../_services/cabinet-configurator.service';
 
 @Component({
   selector: 'app-step-4',
@@ -18,10 +19,10 @@ import {Popover} from 'primeng/popover';
 })
 export class Step4Component implements OnInit {
 
-  @ViewChild('op') op!: Popover;
-
+  cabinetConfiguratorService = inject(CabinetConfiguratorService);
   fb = inject(FormBuilder);
 
+  params = this.cabinetConfiguratorService.getWardrobe();
   stepFourForm: FormGroup = this.fb.group({});
 
   backWallMaterials: Material[] = [
@@ -30,14 +31,35 @@ export class Step4Component implements OnInit {
     {name: 'В четверть', value: 3},
   ];
 
+  list1: Material[] = [
+    {name: 'Шариковые', value:  1},
+    {name: 'Скрытого монтажа', value: 2},
+  ];
 
+  list2: Material[] = [
+    {name: 'AKS', code:  'AKS'},
+    {name: 'AQ (STEALTH soft)', code:  'AQ (STEALTH soft)'},
+    {name: 'Blum', code:  'Blum'},
+    {name: 'Boyard', code:  'Boyard'},
+    {name: 'DTS', code:  'DTS'},
+    {name: 'Firmax', code:  'Firmax'},
+    {name: 'GTV', code:  'GTV'},
+    {name: 'Hettich', code:  'Hettich'},
+  ];
+
+  list3: Material[] = [
+    {name: 'Саморез', value:  1},
+    {name: 'Евровинт', value: 2},
+  ];
 
   ngOnInit(): void {
     this.stepFourForm = this.fb.group({
+      managers: new FormControl<number>(1),
+      fastening: new FormControl<number>(1),
+      manufacturer: new FormControl<string | null>(null),
       backWallMaterial: new FormControl<number>(2),
+      baseColor: new FormControl<string>(''),
+      facadeColor: new FormControl<string>(''),
     });
-    // this.op.align();
   }
-
-
 }
